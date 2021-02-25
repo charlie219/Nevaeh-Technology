9731d02bc4b4dded4b67d1c650330173cbe00e9c
Akash Kumar Bhagat <akashkbhagat221199@gmail.com>
2021-02-25 10:26:43 +0530

    Client-Server-Comunication Added

---
+++ Client-Server-Communication/client.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+#include<sys/socket.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<stdlib.h>
+#include<sstream>
+#include<string.h>
+//#include<chrono>
+
+#define PORT 3112
+using namespace std;
+//using namespace std::chrono;
+
+// CLIENT CODE
+int main(){
+
+	int connection=socket(AF_INET, SOCK_STREAM,0);
+	struct sockaddr_in server_addr;
+	//cout<<sock<<endl;
+	//cout<<"Client Code\n";
+	if(connection<0){
+		cout<<"\n Socket Creation Failed\n";
+		exit(EXIT_FAILURE);
+	}
+	
+	server_addr.sin_family= AF_INET;
+	server_addr.sin_port=htons(PORT);
+
+	if(inet_pton(AF_INET,"127.0.0.1",&server_addr.sin_addr)<=0){
+		cout<<"\n Invalid address\n";
+		exit(EXIT_FAILURE);
+	}
+	if(connect(connection,(struct sockaddr*)&server_addr,sizeof(server_addr))<0){
+		cout<<"Connection Failed\n";
+		exit(EXIT_FAILURE);	
+	}
+	cout<<"----------------------------------------------------------------------------------";
+cout<<"\n\nServer Conection Stablished through PORT: "<<server_addr.sin_port<<endl<<endl;
+	
+//----------------------------------USERNAME PASSWORD-----------------------------
+	
+	char usr[10],pass[10];
+	cout<<"login as: ";
+	cin>>usr;
+	cout<<"Password: ";
+	cin>>pass;
+	
+	send(connection,usr,strlen(usr),0);
+	send(connection,pass,strlen(pass),0);
+	char chk[20];
+	for(int i=0;i<20;i++)
+		chk[i]='\0';
+
+	int xds=read(connection,chk,20);
+	
+	if(strcmp(chk,"OK")!=0){
+	
+		cout<<chk<<endl;
+		close(connection);
+		//main();
+		return 1;
+	}
+	//cout<<chk;
+	char server_msg[80];
+	char client_msg[80]="Greetings from Client...";
+	//usleep(1000);
+	send(connection,client_msg,strlen(client_msg),0);
+	//cout<<"Message Sent::: "<<endl;
+
+	//int val=read(connection,server_msg,80);
+	//cout<<server_msg<<endl<<endl;
+	cout<<"SEND MESSAGE TO SERVER::\n\n";
+	//cin.getline(client_msg,80);
+	//cout<<client_msg<<endl<<endl;
+	while(true){
+		cin.getline(client_msg,80);
+		send(connection,client_msg,strlen(client_msg),0);
+		//char chk2[5];
+		//int sds=read(connection,chk2,5);
+		//cout<<chk2;
+		if(strcmp(client_msg,"quit")==0)
+			break;
+			
+	}
+	cout<<"\n\nServer Disconnected......\n\n";
+	//send(connection,client_msg,strlen(client_msg),0);
+	close(connection);
+	return 1;
+	
+}
---
+++ Client-Server-Communication/server.cpp
@@ -0,0 +1,127 @@
+#include<iostream>
+#include<sys/socket.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+//#include<chrono>
+#include<netinet/in.h>
+#include<sys/time.h>
+#include<iomanip>
+
+#define PORT 3112
+
+using namespace std;
+
+// SERVER CODE
+int main(){
+	struct sockaddr_in addr;			// Socket Adress
+	int optional=1;
+
+	cout<<"------------------------------------------------------------------------\n";
+	cout<<"\n\nSERVER RUNNING\n"<<endl;
+	int conn= socket(AF_INET, SOCK_STREAM,0);
+	//cout<<conn<<endl;
+
+	// Checking if the Socket is failed -> Exit
+	if(conn==0){
+
+		cout<<"socket Failed"<<endl;
+		exit(EXIT_FAILURE);
+	}
+
+	// Initilising setsockopt for reusibility of code
+	if(setsockopt(conn,SOL_SOCKET,SO_REUSEADDR | SO_REUSEPORT,&optional,sizeof(optional))){
+		cout<<"setsockopt";
+		exit(EXIT_FAILURE);
+	}
+	
+	// Initilizing the socket address 
+	addr.sin_family=AF_INET;
+	addr.sin_addr.s_addr= INADDR_ANY;
+	addr.sin_port=htons(PORT);
+
+	
+	// Attaching the Socket to the PORT
+	if(bind(conn,(struct sockaddr*)&addr,sizeof(addr))<0){
+		cout<<"Binding Failed";
+		exit(EXIT_FAILURE);
+	}
+	if(listen(conn,3)<0){
+		cout<<"listening Failed";
+		exit(EXIT_FAILURE);
+	}
+
+	int socket_addr_len=sizeof(addr);
+	int new_socket=accept(conn,(struct sockaddr*)&addr,(socklen_t*)&socket_addr_len);
+	if(new_socket<0){
+		cout<<"Didn't Accept";
+		exit(EXIT_FAILURE);
+	}
+	
+
+	struct timespec st,end;
+	clock_gettime(CLOCK_MONOTONIC,&st);
+	cout<<"\nWaiting For Connection.....\n\n";
+//--------------------------- USERNAME PASWORD------------------------------
+	
+	char usr[10],pass[10];
+	int xdd=read(new_socket,usr,10);
+	xdd=read(new_socket,pass,10);
+	//cout<<usr<<endl<<pass;
+
+	if(strcmp(usr,"akash")!=0 || strcmp(pass,"123455")!=0 ){
+	
+		cout<<"Unknown Client tried to access\n\n";
+		char error[]="Wrong Credentials";
+		send(new_socket,error,strlen(error),0);
+		//main();
+		return 1;
+	}
+
+
+	char empty[80]="";	
+	
+	char client_msg1[80];
+	char server_msg[80]="OK";
+	send(new_socket,server_msg,strlen(server_msg),0);
+	
+	int val=read(new_socket,client_msg1,80);
+	cout<<"---------------------------------------------------------------------------------";
+	cout<<"\n\nClient Connection Stablised\n\n";
+	cout<<client_msg1<<endl<<endl;
+	
+	//char client_msg[80];
+	//cout<<"Send message to server\n\n";
+	//cin>>server_msg;
+	//send(new_socket,server_msg,strlen(server_msg),0);
+	
+	//cout<<"Message Sent"<<endl;
+	//val=read(new_socket,client_msg,80);
+	//cout<<client_msg<<endl<<endl;
+	while(true){
+		clock_gettime(CLOCK_MONOTONIC,&st);
+		char msg[890];
+		for(int i=0;i<80;i++)
+			msg[i]='\0';
+		int xval=read(new_socket,msg,890);
+		clock_gettime(CLOCK_MONOTONIC,&end);
+		double ti=end.tv_sec-st.tv_sec;	
+		cout<<setw(20)<<msg;
+		cout<<setw(20)<<"  -->  Communication delay  "<<ti<<endl;
+
+		if(strcmp(msg,"quit")==0 || ti>10){
+			//char brk[]="YES";
+			//send(new_socket,brk,strlen(brk),0);
+			break;
+		}
+		//char brk[]="NO";
+                //send(new_socket,brk,strlen(brk),0);
+
+
+	}
+	cout<<"\n\n\nDisconnecting Client....\n\n";
+	close(conn);
+	close(new_socket);
+	return 0;
+}
+	 
